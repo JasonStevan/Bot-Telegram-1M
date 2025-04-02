a65ba0e03a41b183453af811cb71241cd0f763fc
JasonStevan <125151719+JasonStevan@users.noreply.github.com>
2025-04-02 11:15:29 -0300

    Update pythonanywhere_wsgi.py

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -1,5 +1,6 @@
 # Este arquivo contém a configuração WSGI para o PythonAnywhere
 import sys
+import os
 
 # Adicione o caminho do seu projeto ao path do Python
 path = '/home/TheBlackWolf/mysite'
@@ -8,6 +9,11 @@ if path not in sys.path:
 
 # Importe a aplicação Flask do seu projeto
 from app import app as application
+
+# Certifique-se de que a pasta de logs existe
+log_dir = os.path.join(path, 'logs')
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
 
 # O PythonAnywhere procura por uma variável chamada 'application'
 # que aponta para sua aplicação Flask
