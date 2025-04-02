dc80138b26f41dfa8c17850a8855bd224e4d91e2
JasonStevan <125151719+JasonStevan@users.noreply.github.com>
2025-04-02 11:08:03 -0300

    Create pythonanywhere_wsgi.py

---
+++ pythonanywhere_wsgi.py
@@ -0,0 +1,13 @@
+# Este arquivo contém a configuração WSGI para o PythonAnywhere
+import sys
+
+# Adicione o caminho do seu projeto ao path do Python
+path = '/home/TheBlackWolf/mysite'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Importe a aplicação Flask do seu projeto
+from app import app as application
+
+# O PythonAnywhere procura por uma variável chamada 'application'
+# que aponta para sua aplicação Flask
